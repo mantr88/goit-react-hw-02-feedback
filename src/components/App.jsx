6d9ff85ec2ed60f3efd1d371228d36bf25e7a3fd
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-04-20 23:38:23 +0300

    add logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
-  return (
+import { Component } from "react";
+
+export class App extends Component  {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+  }
+  
+  addNewFeedback = (e) => {
+    console.dir(e.target.name);
+    switch (e.target.name) {
+      case 'good':
+       return this.setState(pervState => pervState.good += 0.5);
+      case 'neutral':
+       return this.setState(pervState => pervState.neutral += 0.5);
+      case 'bad':
+       return this.setState(pervState => pervState.bad += 0.5);
+      default:
+        throw new Error(`Unknow button name!`);
+    }
+  }
+
+  render() {
+    return(
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'start',
+        flexDirection: "column",
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
-    </div>
-  );
+      <div>
+        <button type="button" name="good" onClick={this.addNewFeedback}>Good</button>
+        <button type="button" name="neutral" onClick={this.addNewFeedback}>Neutral</button>
+        <button type="button" name="bad" onClick={this.addNewFeedback}>Bad</button>
+      </div>
+      <div>
+        <p>Good:{this.state.good}</p>
+        <p>Neutral:{this.state.neutral}</p>
+        <p>Bad:{this.state.bad}</p>
+      </div>
+    </div>)
+    
+  }
 };
