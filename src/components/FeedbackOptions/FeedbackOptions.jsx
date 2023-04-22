bec3094f1eea9f4167a8389020c87ad5f0a3e47d
AntonPetrenko <109439896+mantr88@users.noreply.github.com>
2023-04-22 11:55:27 +0300

    added prop-types

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,4 +1,5 @@
-export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+import PropTypes from 'prop-types';
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
     return (
       <div>{
         options.map(option => (
@@ -13,3 +14,10 @@ export const FeedbackOptions = ({options, onLeaveFeedback}) => {
       </div> 
     );
 };
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+}
